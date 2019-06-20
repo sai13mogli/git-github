1cba027f7c29ba84017fc48347da8297db795438
sai13mogli <sai@moglix.com>
2019-06-20 12:52:10 +0530

    First Developer Program

---
+++ HelloDeveloper.java
@@ -0,0 +1,12 @@
+public class HelloDeveloper{
+
+    public static void main(String[] args){
+    
+     //Prints Hello developer to the Terminal Window
+     System.out.println("Hello Developer");
+     
+
+  }
+
+
+}
